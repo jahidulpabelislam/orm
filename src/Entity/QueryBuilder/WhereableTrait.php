fe68e9b21b3f6fe819b7a2ef796abf1ff334192e
Jahidul Pabel Islam <jahidul@d3r.com>
2025-12-25 01:32:13 +0000

    Allow passing in Entity collection for where clause value

--- src/Entity/QueryBuilder/WhereableTrait.php
+++ src/Entity/QueryBuilder/WhereableTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace JPI\ORM\Entity\QueryBuilder;
 
 use JPI\ORM\Entity;
+use JPI\ORM\Entity\Collection as EntityCollection;
 
 /**
  * Convert the column to full column name if it's a column name, and allow passing entity as value.
@@ -16,7 +17,7 @@ trait WhereableTrait {
     public function where(
         string $whereOrColumn,
         ?string $expression = null,
-        Entity|string|int|float|array|null $valueOrPlaceholder = null
+        EntityCollection|Entity|string|int|float|array|null $valueOrPlaceholder = null
     ): static {
         if ($expression !== null && $valueOrPlaceholder !== null && $this->getEntityInstance()::hasColumn($whereOrColumn)) {
             $whereOrColumn = $this->getEntityInstance()::getFullColumnName($whereOrColumn);
@@ -24,6 +25,9 @@ trait WhereableTrait {
 
         if ($valueOrPlaceholder instanceof Entity) {
             $valueOrPlaceholder = $valueOrPlaceholder->getId();
+        }
+        else if ($valueOrPlaceholder instanceof EntityCollection) {
+            $valueOrPlaceholder = $valueOrPlaceholder->pluck("id")->toArray();
         }
 
         return parent::where($whereOrColumn, $expression, $valueOrPlaceholder);
