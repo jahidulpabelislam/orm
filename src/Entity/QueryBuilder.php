92cc7d2fd665a250a31797f71c272a086a70b0e3
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2025-12-25 15:35:36 +0000

    Clarify comment on skipping ORDER BY for single record

--- src/Entity/QueryBuilder.php
+++ src/Entity/QueryBuilder.php
@@ -55,7 +55,7 @@ class QueryBuilder extends CoreQueryBuilder {
         // Make sure we at least have a consistent order
         if (!count($this->orderBy)) {
             $idColumn = $this->entityInstance::getFullColumnName("id");
-            // Skip ORDER BY when selecting a single record by ID
+            // Skip adding when selecting a single record by ID
             if (count($this->where) !== 1 || (string)$this->where[0] !== "$idColumn = :$idColumn") {
                 $this->orderBy(
                     $this->entityInstance::$defaultOrderByColumn,
