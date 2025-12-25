34d98df5003bf0c8890c8f20fa19f42dc32bac24
Jahidul Pabel Islam <jahidul@d3r.com>
2025-12-25 15:23:39 +0000

    Support Stringable in where

--- src/Entity/QueryBuilder/WhereableTrait.php
+++ src/Entity/QueryBuilder/WhereableTrait.php
@@ -6,6 +6,7 @@ namespace JPI\ORM\Entity\QueryBuilder;
 
 use JPI\ORM\Entity;
 use JPI\ORM\Entity\Collection as EntityCollection;
+use Stringable;
 
 /**
  * Convert the column to full column name if it's a column name, and allow passing entity as value.
@@ -15,9 +16,9 @@ trait WhereableTrait {
     abstract public function getEntityInstance(): Entity;
 
     public function where(
-        string $whereOrColumn,
+        Stringable|string $whereOrColumn,
         ?string $expression = null,
-        EntityCollection|Entity|string|int|float|array|null $valueOrPlaceholder = null
+        EntityCollection|Entity|Stringable|string|int|float|array|null $valueOrPlaceholder = null
     ): static {
         if ($expression !== null && $valueOrPlaceholder !== null && $this->getEntityInstance()::hasColumn($whereOrColumn)) {
             $whereOrColumn = $this->getEntityInstance()::getFullColumnName($whereOrColumn);
